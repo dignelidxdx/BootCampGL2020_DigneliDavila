b9ae7519332bf64fdd6f516a8e27896d28ed695e
Your Name <digneli14@gmail.com>
2020-07-22 21:22:42 +0200

    Ejercicio 4.4: ciclo for iterando del 1 a 10.

--- BootCampGL2020/src/main/java/com/globallogic/app/App.java
+++ BootCampGL2020/src/main/java/com/globallogic/app/App.java
@@ -2,12 +2,10 @@ package com.globallogic.app;
 
 import java.util.Scanner;
 
-import com.globallogic.app.entities.EmpleadoDemo;
-import com.globallogic.app.entities.StudentDetails;
-import com.globallogic.app.entities.Tienda;
-import com.globallogic.app.entities.VariableInstancia;
+import com.globallogic.app.entities.*;
+
 /**
- * Hello world!
+ * Ejercicio Introduccion al Desarrollo
  *
  */
 public class App 
@@ -15,12 +13,14 @@ public class App
     public static Scanner Teclado = new Scanner (System.in);
     public static void main( String[] args )
     {        
-        Tienda tienda = new Tienda();        
-        MensajeUsuario();
-        double pago = Teclado.nextDouble();
-        tienda.promo(pago);
+        CicloFor ciclo = new CicloFor();        
+      
+        ciclo.main();
 
     }
+
+
+
     public static void MensajeUsuario() {
         System.out.println("Ingrese su pago");
     }
--- BootCampGL2020/src/main/java/com/globallogic/app/entities/Tienda.java
+++ BootCampGL2020/src/main/java/com/globallogic/app/entities/Tienda.java
@@ -1,5 +1,21 @@
 package com.globallogic.app.entities;
 
+/*Ejercicio 4.3: Mediantes la cláusula IF resolver la siguiente problemática
+
+En Tienda se hace un 20% de descuento a los clientes cuya compra sea igual a $300, del 25% a los
+clientes cuya compra sea mayor a $350 y sin descuento cuando no se cumple ninguna de las anteriores
+condiciones.
+
+En el caso de tener descuento, imprimir los siguientes mensajes:
+
+El descuento es de: <valor>
+
+En el caso de no tener descuento, imprimir el siguiente mensaje:
+
+Sin descuentos, el total es: <valor>
+
+Ejecutar este ejemplo, tomando como entrada una compra de $400 */
+
 public class Tienda {
     double pagoActual = 0;
     double descuentoUno = 0.80;
