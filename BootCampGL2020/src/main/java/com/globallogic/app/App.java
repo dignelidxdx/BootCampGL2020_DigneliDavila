8d8a5c86d369a796909f5ae20a66562b4dc3778c
Your Name <digneli14@gmail.com>
2020-07-22 04:30:05 +0200

    Terminando el ejercicio 3.5:  se asigno los valores a engPoints y a mathsPoints luego fueron impresos

--- BootCampGL2020/src/main/java/com/globallogic/app/App.java
+++ BootCampGL2020/src/main/java/com/globallogic/app/App.java
@@ -3,6 +3,7 @@ package com.globallogic.app;
 import java.util.Scanner;
 
 import com.globallogic.app.entities.StudentDetails;
+import com.globallogic.app.entities.VariableInstancia;
 /**
  * Hello world!
  *
@@ -12,11 +13,13 @@ public class App
     public static Scanner Teclado = new Scanner (System.in);
     public static void main( String[] args )
     {        
-        MensajeUsuario();
-        int numero = Teclado.nextInt();
-        StudentDetails estudiante = new StudentDetails();
-        estudiante.studentAge(numero);       
 
+        VariableInstancia vI = new VariableInstancia();
+        vI.engPoints = 50;
+        vI.mathsPoints = 80;
+
+        System.out.println("El valor de entProing es: " + vI.engPoints + " y el valor de mathsPoints es: " + vI.mathsPoints);
+      
     }
     public static void MensajeUsuario() {
         System.out.println("Ingrese un numero");
---
+++ BootCampGL2020/src/main/java/com/globallogic/app/entities/VariableInstancia.java
@@ -0,0 +1,13 @@
+package com.globallogic.app.entities;
+
+
+public class VariableInstancia {
+   
+    public int engPoints;
+    public int mathsPoints;
+    public int phyPoints;
+
+    public void main() {
+
+    }
+}
